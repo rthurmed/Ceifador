529e6f58d3de16147210345984724e0dc4e564c5
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-04-30 22:10:47 -0300

    Implemented enemies suffering damage

--- src/enemy/Enemy.gd
+++ src/enemy/Enemy.gd
@@ -5,7 +5,25 @@ const COLLISION_LAYER = 4 # being the enemy
 const COLLISION_MASK = 8 # get hit by player bullets
 const GROUP = 'enemy'
 
+onready var health = $Health
+
+
 func _ready():
 	collision_layer = COLLISION_LAYER
 	collision_mask = COLLISION_MASK
+	
 	add_to_group(GROUP)
+	
+	var _ok
+	_ok = connect("area_entered", self, '_on_Enemy_area_entered')
+	_ok = health.connect("dead", self, '_on_Health_dead')
+
+
+func _on_Enemy_area_entered(area):
+	if area.is_in_group(Bullet.GROUP):
+		health.hit()
+
+
+func _on_Health_dead():
+	queue_free()
+	# TODO: animation?
