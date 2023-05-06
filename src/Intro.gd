43a0f64cbce969477a65e82bd18a22da4e41c73d
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-05-06 01:23:28 -0300

    Added skip button on intro

--- src/Intro.gd
+++ src/Intro.gd
@@ -9,6 +9,15 @@ func _ready():
 	animation.play("intro")
 
 
+func _process(_delta):
+	if Input.is_action_just_released("player_shoot"):
+		go_to_next()
+
+
+func go_to_next():
+	var _ok = get_tree().change_scene("res://src/Main.tscn")
+
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "intro":
-		var _ok = get_tree().change_scene("res://src/Main.tscn")
+		go_to_next()
