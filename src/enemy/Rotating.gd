dc19329f94195fb4e228485c833ecbd73fd4f151
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-06-03 21:42:49 -0300

    Changed rotating attack to happen in steps

--- src/enemy/Rotating.gd
+++ src/enemy/Rotating.gd
@@ -1,8 +1,24 @@
 extends Node2D
 
 
-export var speed_deg = 30 # degrees per seconds
+export var movement_deg = 30 # degrees each time
+export (float, 0.001, 2) var time_slot: float = 0.5
+export (float, 0.0, 2) var time_delay: float = 0.0
+
+onready var timer = $Timer
+
+var delay_passed = false
 
 
-func _process(delta):
-	rotate(deg2rad(speed_deg * delta))
+func _ready():
+	timer.wait_time = time_slot
+	timer.start(time_delay)
+
+
+func _on_Timer_timeout():
+	if not delay_passed:
+		timer.wait_time = time_slot
+		delay_passed = true
+	
+	rotate(movement_deg)
+	
